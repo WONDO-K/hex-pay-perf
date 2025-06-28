ada1c8c0af8e079935548eac0c2b6b6750e4c9bc
최동호 <raspic5874@naver.com>
2025-06-28 23:16:14 +0900

    First Commit

---
+++ src/main/java/com/example/hexpay/hexpay_perf/HexpayPerfApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexpay.hexpay_perf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexpayPerfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexpayPerfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hexpay/hexpay_perf/HexpayPerfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexpay.hexpay_perf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexpayPerfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
